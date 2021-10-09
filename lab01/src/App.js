f0e6d676de4cdbf097ae1ab5ce57ece557758fb5
Michał Kaszubowski <kaszubturbo@gmail.com>
2021-10-09 23:24:26 +0200

    Zadanie 1a

--- lab01/src/App.js
+++ lab01/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [allProducts, setProducts] = useState([]);
+  useEffect(() => {
+    axios
+        .get('https://fakestoreapi.com/products')
+        .then(response => setProducts(response.data))
+  }, []);
+
+  return allProducts.map(product => (
+      <ul>
+          <li>{product.id}</li>
+            <ul>
+                <li>{product.title}</li>
+                <li>{product.price}</li>
+                <li>{product.category}</li>
+            </ul>
+      </ul>
+  ));
 }
 
 export default App;
