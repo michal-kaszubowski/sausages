03ba40f6b7a00d4b6fc313284f70fe09ee1ea929
Michał Kaszubowski <kaszubturbo@gmail.com>
2021-10-21 01:29:57 +0200

    Zadanie 1 -> downgrade. Formularz bez Formika. Kompilacja zachodzi, handleSubmit nie wysyła na API POSTa, tylko odświeża stronę, nie mam pojęcia czemu

--- lab01/src/index.js
+++ lab01/src/index.js
@@ -2,7 +2,6 @@ import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import axios from "axios";
-import {Formik} from "formik";
 
 function List() {
     /** List function component
@@ -13,9 +12,10 @@ function List() {
      * the response. Later this state should be
      * returned as a list of items.
      */
+
     const [all, setAll] = useState([]);
     useEffect(() => {
-        console.log("Sending query to API ...")
+        console.log("Sending query to API ...");
         axios
             .get('https://fakestoreapi.com/products')
             // get returns the promise that should be handled
@@ -23,8 +23,8 @@ function List() {
                 setAll(res.data);
                 console.log(res.status);
             })
-            .catch(err => console.log(err.id));
-        console.log("Query has been send.")
+            .catch(err => console.log(err.status));
+        console.log("GET query has been send.");
     }, []);
 
     const toList = (arr) => arr.map(item => (
@@ -34,11 +34,9 @@ function List() {
          * it to multiple li elements for later
          * return.
          */
+
         <li key={item.id}>
-            <span className="id">{item.id}</span>
-            <span className="title">{item.title}</span>
-            <span className="price">{item.price}</span>
-            <span className="category">{item.category}</span>
+            <span className="id">{item.id}</span> <span className="title">{item.title}</span> <span className="price">{item.price}</span> <span className="category">{item.category}</span>
         </li>
     ));
 
@@ -54,79 +52,69 @@ function Add() {
     /** Add function component
      * This component renders form
      * for user to add new product to API.
-     * Form is controlled by Formik and on
-     * submit is passed through axios post
-     * to remote server.
+     * Form values are controlled by state
+     * and on submit state is passed through
+     * axios post to remote server.
      */
+
+    const [values, setValues] = useState({
+        title: "Product",
+        price: 0,
+        description: "",
+        image: "",
+        category: "default"
+    });
+    const handleChange = event => {
+        setValues({ [event.target.name]: event.target.value });
+        console.log(values);
+    }
+    const handleSubmit = event => {
+        console.log(values)
+        axios
+            .post('https://fakestoreapi.com/products', {
+                title: values.title,
+                price: values.price,
+                description: values.description,
+                image: values.image,
+                category: values.category
+            })
+            .then(res => console.log(res.status))
+            .catch(err => console.log(err.status))
+    }
 
     return (
         <div className="Add">
-            <Formik initialValues={
-                {
-                    description: "",
-                    image: ""
-                }
-            } onSubmit={(values, { setSubmitting }) => {
-                setTimeout(() => {
-                    alert(JSON.stringify(values, null, 2));
-                    setSubmitting(false);
-                }, 400);
-            }}>
-                {({
-                      values,
-                      errors,
-                      touched,
-                      handleChange,
-                      handleSubmit = values => {
-                          axios
-                              .post('https://fakestoreapi.com/products', {
-                                  title: values.title,
-                                  price: values.price,
-                                  description: values.description,
-                                  image: values.image,
-                                  category: values.category
-                              })
-                              .then(res => {
-                                  console.log("POST request has been send.");
-                                  window.location.href="/";
-                              })
-                              .catch(err => {
-                                  console.log(err.status);
-                              });
-                      },
-                      isSubmitting
-                  }) => (
-                      <form onSubmit={handleSubmit(values)}>
-                          <label>
-                              Name:
-                              <input name="title" type="text" onChange={handleChange} value={values.title} required={true} placeholder="SampleProduct"/>
-                          </label>
-                          <label>
-                              Price:
-                              <input name="price" type="text" onChange={handleChange} value={values.price} required={true} placeholder="0.00"/>
-                          </label>
-                          <label>
-                              Description:
-                              <input name="description" type="text" onChange={handleChange} value={values.description} placeholder="Blah blah blah ..."/>
-                          </label>
-                          <label>
-                              Picture:
-                              <input name="image" type="url" onChange={handleChange} value={values.image} placeholder="https://www.samplePage.com"/>
-                          </label>
-                          <label>
-                              Category:
-                              <input name="category" type="text" onChange={handleChange} value={values.category} required={true} placeholder="Antiques"/>
-                          </label>
-                      </form>
-                )}
-            </Formik>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name:
+                    <input name="title" type="text" onChange={handleChange} required={true}/>
+                </label>
+                <label>
+                    Price:
+                    <input name="price" type="number" onChange={handleChange} required={true}/>
+                </label>
+                <label>
+                    Description:
+                    <input name="description" type="text" onChange={handleChange} placeholder="Blah blah blah ..."/>
+                </label>
+                <label>
+                    Picture:
+                    <input name="image" type="url" onChange={handleChange} placeholder="https://www.samplePage.com"/>
+                </label>
+                <label>
+                    Category:
+                    <input name="category" type="text" onChange={handleChange} required={true}/>
+                </label>
+                <button type="submit">Add</button>
+            </form>
         </div>
     );
 }
 
 ReactDOM.render(
     <React.StrictMode>
-        <List/>
+        <List />
+        <Add />
     </React.StrictMode>,
     document.getElementById('root')
 );
