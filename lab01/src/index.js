8c8b7a8a9a465d2cfccdca87eea0b1e2c6f5e17d
Michał Kaszubowski <kaszubturbo@gmail.com>
2021-10-20 17:29:17 +0200

    Praca na labolatoriach nr 3. Nadrabianie nr 1. Uwaga, nie testowane!!!

--- lab01/src/index.js
+++ lab01/src/index.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import axios from "axios";
+import {Formik} from "formik";
 
 function List() {
     /** List function component
@@ -47,6 +48,80 @@ function List() {
         </ul>
     );
 
+}
+
+function Add() {
+    /** Add function component
+     * This component renders form
+     * for user to add new product to API.
+     * Form is controlled by Formik and on
+     * submit is passed through axios post
+     * to remote server.
+     */
+
+    return (
+        <div className="Add">
+            <Formik initialValues={
+                {
+                    description: "",
+                    image: ""
+                }
+            } onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                }, 400);
+            }}>
+                {({
+                      values,
+                      errors,
+                      touched,
+                      handleChange,
+                      handleSubmit = values => {
+                          axios
+                              .post('https://fakestoreapi.com/products', {
+                                  title: values.title,
+                                  price: values.price,
+                                  description: values.description,
+                                  image: values.image,
+                                  category: values.category
+                              })
+                              .then(res => {
+                                  console.log("POST request has been send.");
+                                  window.location.href="/";
+                              })
+                              .catch(err => {
+                                  console.log(err.status);
+                              });
+                      },
+                      isSubmitting
+                  }) => (
+                      <form onSubmit={handleSubmit(values)}>
+                          <label>
+                              Name:
+                              <input name="title" type="text" onChange={handleChange} value={values.title} required={true} placeholder="SampleProduct"/>
+                          </label>
+                          <label>
+                              Price:
+                              <input name="price" type="text" onChange={handleChange} value={values.price} required={true} placeholder="0.00"/>
+                          </label>
+                          <label>
+                              Description:
+                              <input name="description" type="text" onChange={handleChange} value={values.description} placeholder="Blah blah blah ..."/>
+                          </label>
+                          <label>
+                              Picture:
+                              <input name="image" type="url" onChange={handleChange} value={values.image} placeholder="https://www.samplePage.com"/>
+                          </label>
+                          <label>
+                              Category:
+                              <input name="category" type="text" onChange={handleChange} value={values.category} required={true} placeholder="Antiques"/>
+                          </label>
+                      </form>
+                )}
+            </Formik>
+        </div>
+    );
 }
 
 ReactDOM.render(
