0688490d9e357f7fdae8c7c288cfee12600daa8f
Michał Kaszubowski <kaszubturbo@gmail.com>
2021-11-09 00:23:32 +0100

    Zadanie_3/LAB_2

--- lab02/src/index.js
+++ lab02/src/index.js
@@ -2,47 +2,12 @@ import ReactDOM from "react-dom";
 import React from "react";
 import App from "./App";
 import {Provider} from "react-redux";
+import {combineReducers} from "redux";
+import {todoReducer} from "./reducers/todoReducer";
+import {noteReducer} from "./reducers/noteReducer";
 const Redux = require('redux');
 
-/* Action templates:
- * {type: <type>, payload: {id: <id>, title: <title>, done: <done>}}
- */
-
-const initState = {
-    todos: [],
-    notes: []
-}
-
-const reducer = (state = initState, action) => {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return { ...state, todos: [ ...state, action.payload ] };
-        case 'DELETE_TODO':
-            return { ...state, todos: [ ...state.todos.filter(todo => todo.id !== action.payload.id) ] };
-        case 'UPDATE_TODO':
-            return { ...state, todos: [ ...state.todos.reduce(
-                    (acc = [], curr) =>
-                        curr.id === action.payload.id
-                            ? [ ...acc, { ...curr, title: action.payload.title }]
-                            : [ ...acc, curr ]
-                ) ] };
-        case 'FINISH_TODO':
-            return { ...state, todos: [ ...state.todos.reduce(
-                    (acc = [], curr) =>
-                        curr.id === action.payload.id
-                            ? [ ...acc, { ...curr, done: true }]
-                            : [ ...acc, curr ]
-                ) ] };
-        case 'ADD_NOTE':
-            return { ...state, notes: [ ...state, action.payload ] };
-        case 'DELETE_NOTE':
-            return { ...state, notes: [ ...state.notes.filter(note => note.id !== action.payload.id) ] };
-        default:
-            return state;
-    }
-}
-
-const store = Redux.createStore(reducer)
+const store = Redux.createStore( combineReducers( { todos: todoReducer, notes: noteReducer } ) );
 
 ReactDOM.render(
     <React.StrictMode>
