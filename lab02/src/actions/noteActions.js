ad1851ba7b0dfc5d5d771f65e1460c6074c95ab3
Michał Kaszubowski <kaszubturbo@gmail.com>
2021-11-09 01:26:09 +0100

    Dodanie akcji, jako funkcji; koniec Laboratorium_2

---
+++ lab02/src/actions/noteActions.js
@@ -0,0 +1,9 @@
+export const addNote = (payload) => ({
+    type: 'ADD_NOTE',
+    payload
+});
+
+export const deleteNote = (payload) => ({
+    type: 'DELETE_NOTE',
+    payload
+});
---
+++ lab02/src/actions/todoActions.js
@@ -0,0 +1,19 @@
+export const addTodo = (payload) => ({
+    type: 'ADD_TODO',
+    payload
+});
+
+export const deleteTodo = (payload) => ({
+    type: 'DELETE_TODO',
+    payload
+});
+
+export const updateTodo = (payload) => ({
+    type: 'UPDATE_TODO',
+    payload
+});
+
+export const finishTodo = (payload) => ({
+    type: 'FINISH_TODO',
+    payload
+});
