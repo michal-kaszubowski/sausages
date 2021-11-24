c6befefa93ccbfdd1d0175b5728bbbc3343d0bd6
Michał Kaszubowski <kaszubturbo@gmail.com>
2021-11-25 00:40:06 +0100

    Dodanie elementu dodającego film/dyrektora. UWAGA -> ERRORy -> Komponent zmienia stan z niekontrolowanego na kontrolowany. Zmiana selecta zmienia typ, ale jest on nieodpowiedni.

---
+++ lab04/src/Add.js
@@ -0,0 +1,84 @@
+import {Field, Form, Formik} from "formik";
+import {useDispatch} from "react-redux";
+import {v4} from "uuid";
+import {addMovie, addDirector} from "./ducks/actions";
+
+function Add(props) {
+    const dispatch = useDispatch();
+
+    const initiateValues = () => {
+        switch (props.itemType) {
+            case "movie":
+                return {
+                    title: "",
+                    year: 2000
+                }
+            case "director":
+                return {
+                    firstName: "",
+                    lastName: "",
+                    age: 0
+                }
+            default:
+                return console.error("Error! Mismatching itemType, initiation of values failed.")
+        }
+    }
+
+    const handleSubmit = (values, actions) => {
+        switch (props.itemType) {
+            case "movie":
+                try {
+                    dispatch(addMovie({id: v4(), ...values}));
+                    return actions.resetForm();
+                } catch (error) {
+                    return console.error(error);
+                }
+            case "director":
+                try {
+                    dispatch(addDirector({id: v4(), ...values}));
+                    return actions.resetForm();
+                } catch (error) {
+                    return console.error(error);
+                }
+            default:
+                return console.error("Error! Mismatching itemType, dispatching values failed.")
+        }
+    }
+
+    const fields = () => {
+        switch (props.itemType) {
+            case "movie":
+                return (
+                    <Form>
+                        <Field name="title" type="text"/>
+                        <Field name="year" type="number"/>
+                        <button type="submit">Add</button>
+                    </Form>
+                );
+            case "director":
+                return (
+                    <Form>
+                        <Field name="firstName" type="text"/>
+                        <Field name="lastName" type="text"/>
+                        <Field name="age" type="number"/>
+                        <button type="submit">Add</button>
+                    </Form>
+                );
+            default:
+                return console.error("Error! Mismatching itemType, generating form impossible.")
+        }
+    }
+
+    return (
+        <div className="Add">
+            <Formik
+                initialValues={initiateValues}
+                onSubmit={handleSubmit}
+            >
+                {fields}
+            </Formik>
+        </div>
+    );
+}
+
+export default Add
--- lab04/src/App.js
+++ lab04/src/App.js
@@ -1,7 +1,18 @@
+import Add from "./Add";
+import {useState} from "react";
+
 function App() {
+    const [itemType, setItemType] = useState("movie");
+
     return (
-        <div className="App">
-            ...
+        <div className="app">
+            <div className="add">
+                <select name="itemTypeSelect" onChange={event => setItemType(event.target.value)}>
+                    <option>Movie</option>
+                    <option>Director</option>
+                </select>
+                <Add itemType={itemType}/>
+            </div>
         </div>
     );
 }
