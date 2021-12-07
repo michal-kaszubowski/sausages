90c8699fe85a4bc27adcd39337676ec47feb6228
Michał Kaszubowski <kaszubturbo@gmail.com>
2021-12-07 02:19:41 +0100

    Czegoś mi tu brakowało...

--- lab04/src/Navbar.js
+++ lab04/src/Navbar.js
@@ -3,6 +3,7 @@ import "./ui/Navbar.css";
 const Navbar = () => {
     return (
         <div className="Navbar">
+            <div className="home">Home</div>
             <div className="movies">Movies</div>
             <div className="directors">Directors</div>
             <div className="add">Add</div>
