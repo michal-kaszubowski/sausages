460cb214027fc3c5e624331fc13dfc93b3516824
Michał Kaszubowski <kaszubturbo@gmail.com>
2021-12-09 00:04:57 +0100

    Dodanie loggera z dokumentacji i podłączenie go do stora

--- lab04/src/ducks/store.js
+++ lab04/src/ducks/store.js
@@ -1,4 +1,4 @@
-import {combineReducers, createStore} from "redux";
+import {combineReducers, createStore, applyMiddleware} from "redux";
 import {movieReducer} from "./movies/reducers";
 import {directorReducer} from "./directors/reducers";
 
@@ -7,4 +7,13 @@ const oneReducer = combineReducers({
     directors: directorReducer
 });
 
-export const store = createStore(oneReducer);
+const logger = store => next => action => {
+    console.group(action.type)
+    console.info('Dispatching action:', action)
+    const result = next(action)
+    console.log('Next state:', store.getState())
+    console.groupEnd()
+    return result
+}
+
+export const store = createStore(oneReducer, applyMiddleware(logger));
