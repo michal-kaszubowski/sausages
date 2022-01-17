c3c301e72b973ae6ae9008bec695d0f4f20fd424
Michał Kaszubowski <kaszubturbo@gmail.com>
2022-01-17 19:30:19 +0100

    List needs details. Preparing to route

--- projekt/frontend/src/components/List.js
+++ projekt/frontend/src/components/List.js
@@ -1,10 +1,20 @@
 import {connect} from "react-redux";
+import picture from "../sausage.jpg";
 
 const List = ({state}) => {
     return (
         <div className="List">
             {state.sausages.map(sausage => (
-                <div key={sausage._id}>.</div>
+                <div key={sausage._id}>
+                    {sausage.image
+                        ? (<img src={sausage.image} alt={sausage.type}/>)
+                        : (<img src={picture} alt={"obrazek z kiełbaskami"}/>)
+                    }
+                    <div>{sausage.type}</div>
+                    <div>{sausage.price}</div>
+                    <div>{sausage.manufacturer.name}</div>
+                    <button className={"details"}>...</button>
+                </div>
             ))}
         </div>
     );
